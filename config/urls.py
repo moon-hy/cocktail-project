c2809aa19a865a286e7696a6b904f820fcb5a76d
hy.moon <moraramee@gmail.com>
2022-03-23 18:19:57 +0900

    Added cocktail APi

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ingredient.urls')),
+    path('api/', include('cocktail.urls')),
 ]
