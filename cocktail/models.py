ae3d36567c983ec4840b308fc2fc30a59987bda0
hy.moon <moraramee@gmail.com>
2022-03-24 01:50:27 +0900

    Added glass field

--- cocktail/models.py
+++ cocktail/models.py
@@ -11,12 +11,27 @@ COCKTAIL_BASE = (
     'wine',
     'mixed',
 )
+COCKTAIL_GLASS = (
+    'any',
+    'hot-drink', 
+    'collins', 
+    'highball', 
+    'martini', 
+    'hurricane', 
+    'white-wine', 
+    'old-fashioned', 
+    'margarita', 
+    'shot', 
+    'champagne-tulip', 
+    'champagne-flute'
+)
 RECIPE_UNIT = (
     'ml',
     'oz',
     'dashes',
     'drops',
     'fill-up',
+    'bsp',
     'leaves',
 )
 class Tag(models.Model):
@@ -29,19 +44,19 @@ class Recipe(models.Model):
     UNIT_CHOICES = [
         (i+1, base) for i, base in enumerate(RECIPE_UNIT)
     ]
-    cocktail = models.ForeignKey(
+    cocktail    = models.ForeignKey(
         'Cocktail',
         related_name='recipe',
         on_delete=models.CASCADE,
         blank=True
     )
-    ingredient = models.ForeignKey(
+    ingredient  = models.ForeignKey(
         'ingredient.Ingredient',
         related_name='recipe',
         on_delete=models.CASCADE
     )
-    volume = models.PositiveSmallIntegerField()
-    unit = models.PositiveSmallIntegerField(choices=UNIT_CHOICES)
+    volume      = models.PositiveSmallIntegerField()
+    unit        = models.PositiveSmallIntegerField(choices=UNIT_CHOICES)
 
     def __str__(self):
         return self.cocktail.name
@@ -50,9 +65,14 @@ class Cocktail(models.Model):
     BASE_CHOICES = [
         (i+1, base) for i, base in enumerate(COCKTAIL_BASE)
     ]
-    name = models.CharField(max_length=255)
-    base = models.PositiveSmallIntegerField(choices=BASE_CHOICES)
-    garnish = models.CharField(max_length=255, blank=True)
+    GLASS_CHOICES = [
+        (i+1, glass) for i, glass in enumerate(COCKTAIL_GLASS)
+    ]
+
+    name        = models.CharField(max_length=255)
+    base        = models.PositiveSmallIntegerField(choices=BASE_CHOICES)
+    glass       = models.PositiveSmallIntegerField(choices=GLASS_CHOICES, default=1)
+    garnish     = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
     ingredients = models.ManyToManyField(
         'ingredient.Ingredient',
