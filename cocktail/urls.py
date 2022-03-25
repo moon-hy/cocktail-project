83f19cdc214b88db87dab6996b7641f20b6398ac
hy.moon <moraramee@gmail.com>
2022-03-25 20:24:51 +0900

    Refactored urls.py

--- cocktail/urls.py
+++ cocktail/urls.py
@@ -6,6 +6,6 @@ from cocktail.views import *
 urlpatterns = [
     path('cocktails', CocktailList.as_view(), name='cocktail_list'),
     path('cocktails/<int:pk>', CocktailDetail.as_view(), name='cocktail_detail'),
-    path('cocktails/tag', CocktailTag.as_view(), name='cocktail_tag_list'),
-    path('cocktails/tag/<str:pk>', CocktailListByTag.as_view(), name='cocktail_list_tag')
+    path('cocktails/tags', CocktailTag.as_view(), name='cocktail_tag_list'),
+    path('cocktails/tags/<str:pk>', CocktailListByTag.as_view(), name='cocktail_list_tag')
 ]
