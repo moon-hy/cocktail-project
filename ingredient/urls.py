0420cdcb3fbc10f3476914497ae57370d089a7f9
hy.moon <moraramee@gmail.com>
2022-03-25 21:23:58 +0900

    Refactored urls.py

--- ingredient/urls.py
+++ ingredient/urls.py
@@ -4,8 +4,6 @@ from ingredient.views import *
 
 
 urlpatterns = [
-    path('categories', CategoryList.as_view(), name='category_list'),
     path('ingredients', IngredientList.as_view(), name='ingredient_list'),
     path('ingredients/<int:pk>', IngredientDetail.as_view(), name='ingredient_detail'),
-    path('ingredients/category/<int:pk>', IngredientCategoryList.as_view(), name='ingredient_category_list'),
 ]
