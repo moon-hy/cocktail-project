e6d99dcd31eb44a5d436a59b37bb352bd4063c5b
hy.moon <moraramee@gmail.com>
2022-03-30 10:19:06 +0900

    Added query logging

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            }
+        }
+    }
